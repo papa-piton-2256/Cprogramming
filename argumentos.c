e6ec4c018a463791df66dc63d76c7952e9843bb9
papa-piton-2256 <ingenieroandresfie@gmail.com>
2016-03-30 20:00:16 -0600

    Update argumentos.c
    Parameters usage in C

--- argumentos.c
+++ argumentos.c
@@ -1,4 +1,18 @@
+
+/*Compile gcc foo.c -std=c99*/
+
 #include<stdio.h>
 int main(int argc,char** argv){
-
+  
+      printf("There are %d arguments\n",argc);
+      // ./a.out we are the champions
+      printf("The arguments are\n");
+      for(int i=1;i<argc;i++){
+           printf("%s\n",argv[i]);
+      }
+      //we
+      //are
+      //the
+      //champions
+  
 }
